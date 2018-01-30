370ac5f984ba7795904f7bc18b3a270313b607f4
Diana Norgaard <dnorgaard@usgs.gov>
2018-01-30 14:50:00 -0800

    update make script

--- make_exe.bat
+++ make_exe.bat
@@ -1,3 +1,3 @@
 rem pyinstaller --add-data="site-packages\obspy\imaging\data\*npz;site-packages\obspy\imaging\data" --onedir src\RsamSsam.py --distpath dist2 
 
-pyinstaller --add-data="site-packages\obspy\imaging\data\*npz;site-packages\obspy\imaging\data" --onefile src\RsamSsam.py --distpath dist
+pyinstaller --onefile src\RsamSsam.py --distpath dist
