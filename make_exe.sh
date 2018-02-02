fe0e11a3271887d5e90439b09da36c52b657f159
dnorgaard <dnorgaard@192.168.0.100>
2018-02-02 09:15:33 -0800

    change line

--- make_exe.sh
+++ make_exe.sh
@@ -2,5 +2,5 @@
 
 
 #pyinstaller -n Rsam2Disk --clean --hidden-import=scipy._lib.messagestream --onefile src/RsamSsam.py --distpath dist
-#pyinstaller -n Rsam2Disk --clean --onefile src/RsamSsam.py --distpath dist
-pyinstaller Rsam2Disk.spec
+pyinstaller -n Rsam2Disk --clean --onefile src/RsamSsam.py --distpath dist
+#pyinstaller Rsam2Disk.spec
